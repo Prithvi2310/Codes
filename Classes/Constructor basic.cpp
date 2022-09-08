49c7ace1fd30807bb50342ec819def5f64b60bb8
Prithvi2310 <103898139+Prithvi2310@users.noreply.github.com>
2022-09-08 14:05:46 +0530

    Create Constructor basic.cpp

---
+++ Classes/Constructor basic.cpp
@@ -0,0 +1,37 @@
+//Decimal to binary conversion using constructor.
+
+#include <iostream>
+
+using namespace std;
+
+class binary{
+    public:
+        int i=1, binaryNum=0;
+        binary(int num){
+            while(num){
+              //Using the logic of the power of 10 in any integer.
+                binaryNum += (num%2) * i;
+                i *= 10;
+                num /= 2;
+            }
+            cout<<"Binary equivalent of the input decimal number: "<<binaryNum<<endl;
+        }
+};
+
+int main()
+{
+    int n,flag=1;
+    while(flag){
+        cout<<"Enter the decimal number to be converted: ";
+        cin>>n;
+        binary b1(n);
+        cout<<"\nEnter 0 to quit and 1 to continue: ";
+        cin>>flag;
+        cout<<endl;
+        if(flag==1)
+            continue;
+        else if(flag==0)
+            flag==0;
+    }
+    return 0;
+}
